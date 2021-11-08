114471d362f3bc9bf3b671a3f6ae02426a40afe5
conniekang <hqk1121@gmail.com>
2021-11-07 20:58:29 -0500

    commit tree c8f22765d629ed57fb3b06dcc37031fa8171300a

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+  def actors_list
+      res = []
+      self.characters.each do |c|
+        res << Actor.find(c.actor_id).full_name
+      end
+      res
+  end
 end
